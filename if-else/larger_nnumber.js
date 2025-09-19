edbf113b5fc200cb57005edf91ba3b6635ccb4d9
ItsSidhartha <sidharthamaji04@gmail.com>
2025-09-19 14:06:44 +0530

    storing the code to find the larger no of the given two nos

---
+++ if-else/larger_nnumber.js
@@ -0,0 +1,11 @@
+const firstInput = 23;
+const secondInput = 32;
+
+if (firstInput < secondInput) {
+    console.log(firstInput, 'is less than',secondInput);
+} else if (secondInput > firstInput) {
+    console.log(firstInput, 'is not less than', secondInput);
+} else {
+    console.log(firstInput, 'is equal to', secondInput);
+}
+ 
