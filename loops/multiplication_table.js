afa990ac1bfa8551a0fd8748bd33c636f5a8c2ae
ItsSidhartha <sidharthamaji04@gmail.com>
2025-09-19 13:55:29 +0530

    storing the code for printing the multiplication table of a given number

---
+++ loops/multiplication_table.js
@@ -0,0 +1,8 @@
+const number = 5;
+let multiplier = 1;
+const finalMultiplier = 10;
+
+while(multiplier <= finalMultiplier){
+    console.log(number, '*', multiplier, '=', number * multiplier);
+    multiplier = multiplier + 1;
+}
