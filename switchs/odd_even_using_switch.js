05067070e43c5725f3346ccb7230e7decc1795c3
ItsSidhartha <sidharthamaji04@gmail.com>
2025-09-19 13:51:14 +0530

    storing the code for checking the given no is odd or even using switch

---
+++ switchs/odd_even_using_switch.js
@@ -0,0 +1,11 @@
+const number = 12;
+const remainder = number % 2
+
+switch(remainder){
+    case 0 :
+        console.log(number, " is even");
+        break;
+    case 1 :
+        console.log(number, " is odd");
+        break;
+}
