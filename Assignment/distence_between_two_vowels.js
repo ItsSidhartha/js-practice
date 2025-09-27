8e33702c85a2d7f2d1f98949ef612d5dd3f28149
ItsSidhartha <sidharthamaji04@gmail.com>
2025-09-27 21:57:33 +0530

    solving for string with vowels in index of more than 10

--- Assignment/distence_between_two_vowels.js
+++ Assignment/distence_between_two_vowels.js
@@ -1,4 +1,19 @@
-function vowelsAt(string) {
+function isVowel(letter) {
+  switch (letter) {
+    case "a" : return true;
+    case "e" : return true;
+    case "i" : return true;
+    case "o" : return true;
+    case "u" : return true;
+    default : return false;
+  }
+}
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+/*function vowelsAt(string) {
   const vowels = "aeiou";
   let indicesOfVowels = "";
 
@@ -15,27 +30,47 @@ function vowelsAt(string) {
       vowelIndex++;
     }
   }
-
   return indicesOfVowels;
 }
 
-function calculateDistence(string) {
+function calculateMinDistence(string) {
   const indicesOfVowels = vowelsAt(string);
 
   if (indicesOfVowels.length <= 1) {
     return -1;
   }
-  
+
   let minDistance = Infinity;
 
   for (let index = indicesOfVowels.length - 1; index > 0; index--) {
     const distance = indicesOfVowels[index] - indicesOfVowels[index - 1];
-    
+
     if (distance < minDistance) {
       minDistance = distance;
     }
 
   }
+  return minDistance;
+}
+*/
+
+function distanceBetweenTwoVowels(string) {
+  let currVowelIndex = -1;
+  let nextVowelIndex = -1;
+  let minDistance = Infinity;
+
+  for (let stringIndex = 0; stringIndex < string.length; stringIndex++) {
+    if (isVowel(string[stringIndex])) {
+      currVowelIndex = nextVowelIndex; 
+      nextVowelIndex = stringIndex;
+      minDistance = currVowelIndex === -1 ? minDistance : min(minDistance, nextVowelIndex - currVowelIndex);
+    }
+  }
+
+  if (minDistance === Infinity) {
+    return -1;
+  }
+
   return minDistance;
 }
 
@@ -54,7 +89,7 @@ function composseMessage(actual, expected) {
 }
 
 function testCalculateDistence(string, expected) {
-  const actual = calculateDistence(string);
+  const actual = distanceBetweenTwoVowels(string);
 
   console.log(composseMessage(actual, expected));
 }
@@ -67,9 +102,10 @@ function testAll() {
   testCalculateDistence("abyss", -1);
   testCalculateDistence("abbbba", 5);
   testCalculateDistence("abbabba", 3);
-  testCalculateDistence("abbbbabba", 3);
+  testCalculateDistence("abcdefghijklmnopqrstuvwxyzaa", 1);
   testCalculateDistence("aba", 2);
   testCalculateDistence("abbaa", 1);
+  testCalculateDistence("", -1);
 }
 
 testAll();
