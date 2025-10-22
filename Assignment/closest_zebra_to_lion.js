8c606d69aca15b1ce385d9c02ed0ccb949a6788c
ItsSidhartha <sidharthamaji04@gmail.com>
2025-10-22 23:06:40 +0530

    Update closest_zebra_to_lion.js

--- Assignment/closest_zebra_to_lion.js
+++ Assignment/closest_zebra_to_lion.js
@@ -1,45 +1,71 @@
 const testCase1 = "LZ";
-const testCase2 = "Z L";
+const testCase2 = "Z  L";
 const testCase3 = "L     Z";
 const testCase4 = "L     L";
 const testCase5 = "Z  Z  Z";
 const testCase6 = "L  ZL Z";
 const testCase7 = "Z   LL Z";
-const testCase8 = "ZZ L  Z";
-const testCase9 = "Z  LZZ LLZZ  LL   ZZ    L";
-const testCase10 = "ZZZZZZ   LLLLLL"
-const testCase11 = " Z LL  ZL"
-const testCase12 = "   LLLLLL   Z"
-const testCase13 = "  L Z  ZZ  LL"
-const testCase14 = "   L    L"
-const testCase15 = "  Z   L Z"
 
+function savana(field) {
+  let minDistance = Infinity;
 
-const testCaseToUse = testCase15;
-let indexOfCurrentTargetAnimal = 0;
-let indexOfTestCase = 0;
-let currentTargetAnimal = "";
-while (!currentTargetAnimal && indexOfTestCase < testCaseToUse.length) {
-    if (testCaseToUse[indexOfTestCase] !== " ") {
-        currentTargetAnimal = testCaseToUse[indexOfTestCase];
-        indexOfCurrentTargetAnimal = indexOfTestCase;
+  for (let currIndex = 0; currIndex < field.length; currIndex++) {
+    if (field[currIndex] !== ' ') {
+      const currAnimal = field[currIndex];
+      const targetAnimal = currAnimal === 'Z' ? 'L' : 'Z';
+      const indexOfClosestTarget = field.indexOf(targetAnimal, currIndex);
+      const targetIndex = indexOfClosestTarget === -1 ? Infinity : indexOfClosestTarget;
+      const distance = targetIndex - currIndex;
+      minDistance = Math.min(distance, minDistance);
     }
-    indexOfTestCase = indexOfTestCase + 1;
+  }
+
+  return minDistance === Infinity ? -1 : minDistance;
 }
 
-let minimumDistanceBetweenTwoAnimals = -1;
 
-for (let distanceBetweenTwoAnimals = Infinity; indexOfTestCase < testCaseToUse.length; indexOfTestCase++) {
-    if (testCaseToUse[indexOfTestCase] === currentTargetAnimal) {
-        indexOfCurrentTargetAnimal = indexOfTestCase;
-    } else if (testCaseToUse[indexOfTestCase] !== currentTargetAnimal && testCaseToUse[indexOfTestCase] !== " ") {
-        distanceBetweenTwoAnimals = indexOfTestCase - indexOfCurrentTargetAnimal - 1;
-        if (minimumDistanceBetweenTwoAnimals > distanceBetweenTwoAnimals || minimumDistanceBetweenTwoAnimals < 0) {
-            minimumDistanceBetweenTwoAnimals = distanceBetweenTwoAnimals;
-        }
-        currentTargetAnimal = testCaseToUse[indexOfTestCase];
-        indexOfCurrentTargetAnimal = indexOfTestCase;
-    }
+function validateOutput(actual, expected) {
+  return actual === expected;
 }
 
-console.log("Input: ", testCaseToUse, "Output: ", minimumDistanceBetweenTwoAnimals );
+function composeMessageForSuccess(testType) {
+  return `✅ ${testType}`;
+}
+
+function composeMessageForFail(inputs, testType, actual, expected) {
+  const message = `\n❌ ${testType}
+      | inputs - [${inputs}]
+      | expected output - [${expected}]
+      | actual output - [${actual}]\n`;
+  return message;
+}
+
+
+function composeMessage(inputs, actual, expected, testType) {
+  let message = "";
+
+  if (validateOutput(actual, expected)) {
+    message = composeMessageForSuccess(testType);
+  } else {
+    message = composeMessageForFail(inputs, testType, actual, expected);
+  }
+
+  return message;
+}
+
+function testSavana(inputs, expected, testType) {
+  const actual = savana(inputs);
+  console.log(composeMessage(inputs, actual, expected, testType));
+}
+
+function testAll() {
+  testSavana(testCase1, 1, "one of each animal");
+  testSavana(testCase2, 3, "seperated L and Z");
+  testSavana(testCase3, 6, "seperated Z and L");
+  testSavana(testCase4, -1, "only L");
+  testSavana(testCase5, -1, "only Z");
+  testSavana(testCase6, 1, "pair in the middle");
+  testSavana(testCase7, 2, "pair at the end");
+}
+
+testAll();
