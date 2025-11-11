8c6acc1e2bf302f16341c92392f5061b5ce7b70d
Sidhartha Maji <sidharthamaji@Sidharthas-MacBook-Pro.local>
2025-11-11 13:01:48 +0530

    frequency in object

---
+++ objects/frequency.js
@@ -0,0 +1,13 @@
+const data = [12, 12, 45, 5, 4, 34, 2, 41, 4,12];
+
+const frequencyData = (frequency, target) => {
+  if (!(target in frequency)) {
+    frequency[target] = 0;
+  }
+
+  frequency[target]++;
+  return frequency;
+};
+const frequencytable = data.reduce(frequencyData, {});
+
+console.log(frequencytable);
